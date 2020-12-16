7c44ce3ca46a299e372c49db29f7bdfdbb42577d
Qasim Ali <33759178+7qasimaligujjar@users.noreply.github.com>
2020-12-16 21:21:35 +0500

    Add files via upload

---
+++ lab09/lab09/components/AssetExample.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+export default function AssetExample() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+        Hello, I am Muhammad Qasim Ali.
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+  },
+  paragraph: {
+    margin: 24,
+    marginTop: 0,
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
